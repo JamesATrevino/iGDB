654f7db40e8554fd406b3454edca1c3575467e34
James Trevino <JamesA.Trevino@gmail.com>
2016-03-22 11:58:39 -0500

    Added new Login Controller

---
+++ iGDB/iGDB/LogInRegistrationViewController.swift
@@ -0,0 +1,40 @@
+//
+//  LogInRegistrationViewController.swift
+//  iGDB
+//
+//  Created by James Trevino on 3/22/16.
+//  Copyright © 2016 cs378. All rights reserved.
+//
+
+import UIKit
+
+class LogInRegistrationViewController: UIViewController {
+
+    @IBOutlet weak var accountTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func loginPressed(sender: AnyObject) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
