a9e786b37d61d627743656b076488ab71d76535d
James Trevino <JamesA.Trevino@gmail.com>
2016-03-23 14:37:16 -0500

    added images where available

--- iGDB/iGDB/GameDetailViewController.swift
+++ iGDB/iGDB/GameDetailViewController.swift
@@ -15,18 +15,49 @@ class GameDetailViewController: UIViewController {
     @IBOutlet weak var metacriticLabel: UILabel!
     @IBOutlet weak var studioLabel: UILabel!
     @IBOutlet weak var summaryLabel: UITextView!
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Do any additional setup after loading the view.
         let name = game!["name"]
         self.title = name as? String
         yearLabel.text = String(game!["year"] as! Int)
         platformsLabel.text = game!["platform"] as? String
-        metacriticLabel.text = String(game!["rating"] as! Double)
+        let metaCritic = game!["rating"] as? Double
+        metacriticLabel.text = "No metacritic data availible"
+        if metaCritic != nil
+        {
+            metacriticLabel.text = String(metaCritic!)
+        }
         studioLabel.text = game!["developer"] as? String
         summaryLabel.text = game!["Summary"] as? String
         summaryLabel.editable = false
-
-        // Do any additional setup after loading the view.
+        let url = game!["image"] as? String
+        if url != "not set"
+        {
+            let checkedUrl = NSURL(string: url!)
+            imageView.contentMode = .ScaleAspectFit
+            downloadImage(checkedUrl!)
+        }
+    }
+    
+    func getDataFromUrl(url:NSURL, completion: ((data: NSData?, response: NSURLResponse?, error: NSError? ) -> Void)) {
+        NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) in
+            completion(data: data, response: response, error: error)
+            }.resume()
+    }
+    
+    func downloadImage(url: NSURL){
+        print("Download Started")
+        print("lastPathComponent: " + (url.lastPathComponent ?? ""))
+        getDataFromUrl(url) { (data, response, error)  in
+            dispatch_async(dispatch_get_main_queue()) { () -> Void in
+                guard let data = data where error == nil else { return }
+                print(response?.suggestedFilename ?? "")
+                print("Download Finished")
+                self.imageView.image = UIImage(data: data)
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
